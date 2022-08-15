1d95ea30b95088a495f72f878e57d83de4227f3a
Benson <benson.kurian@elogosoft.in>
2022-08-15 16:36:57 +0530

    redirecting and session management

--- src/main/java/com/demoapp/SqServlet.java
+++ src/main/java/com/demoapp/SqServlet.java
@@ -6,11 +6,14 @@ import java.io.PrintWriter;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
-		int k = Integer.parseInt(req.getParameter("k"));
+		HttpSession session = req.getSession();
+		
+		int k = (int) session.getAttribute("k");
 		
 		k = k * k;
 		
